e5230911539c117ff72ad27fb01a3c18fc8bfb28
unknown <hector_corrales_66@hotmail.com>
2024-07-09 16:44:06 -0700

    agregue archivos

---
+++ src/ConsultaMoneda/ConsultarMoneda.java
@@ -0,0 +1,5 @@
+package ConsultaMoneda;
+
+public class ConsultarMoneda {
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+import ConsultaMoneda.ConsultarMoneda;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        ConsultarMoneda consulta = new ConsultarMoneda();
+
+        int opcion=0;
+        while (opcion != 8){
+            System.out.println("*******************************\n"; +
+                    "Bienvenido al convertidor de moneda\n\n" +
+                    "Ingresa la conversion que deseas realizar\n\n" +
+                    
+        }
+
+    }
+}
